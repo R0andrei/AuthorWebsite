46b222b57f3501a96acc779cd81db045ee35f9a9
R0andrei <108833337+R0andrei@users.noreply.github.com>
2022-07-12 12:03:24 +0300

    Add files via upload

--- dropdown.js
+++ dropdown.js
@@ -1,19 +1,24 @@
 //dropdown button global
-$(document).ready(function(){
-$("#dbutton").hover(function(){
-    $("#drop").toggle()
+
+$(document).ready(function () {
+    $("#dbutton").hover(function () {
+        $("#drop").toggle()
     });
 });
+
 //dropdown button mobile
-$(document).ready(function(){
-    $(".burger").click(function(){
+
+$(document).ready(function () {
+    $(".burger").click(function () {
         $(".nav-mob").toggle()
     });
 });
+
 //carousel
+
 var slideIndex = 1;
 showSlides(slideIndex);
-sTimer = setInterval(function(){auto()}, 4000);
+sTimer = setInterval(function () { auto() }, 4000);
 function auto() {
     slideIndex++;
     if (slideIndex > 3) { slideIndex = 1 }
@@ -23,7 +28,7 @@ function auto() {
 function currentSlide(n) {
     clearInterval(sTimer);
     showSlides(slideIndex = n);
-    sTimer = setInterval(function(){auto()}, 4000);
+    sTimer = setInterval(function () { auto() }, 4000);
 }
 
 function showSlides(x) {
@@ -35,7 +40,19 @@ function showSlides(x) {
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
-      }
+    }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    dots[slideIndex - 1].className += " active";
+}
+
+// Form validation
+
+function sendMail(mail) {
+    let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (mail.value.match(mailFormat)) {
+        alert("Mesaj trimis");
+    }
+    else {
+        alert("Adresa de email incorecta");
+    }
 }
